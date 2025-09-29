7fbaeca30dcc520c3b7d27260cee76e86d3164c4
Pekka Enberg <penberg@iki.fi>
2025-09-29 11:09:39 +0300

    Add debug logging to Antithesis runs

--- antithesis-tests/stress-io_uring/singleton_driver_stress.sh
+++ antithesis-tests/stress-io_uring/singleton_driver_stress.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/bin/turso_stress --silent --nr-threads 2 --nr-iterations 10000 --vfs io_uring
+RUST_BACKTRACE=1 RUST_LOG=debug /bin/turso_stress --silent --nr-threads 2 --nr-iterations 10000 --vfs io_uring
--- antithesis-tests/stress-unreliable/singleton_driver_stress.sh
+++ antithesis-tests/stress-unreliable/singleton_driver_stress.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-LD_PRELOAD=/usr/lib/unreliable-libc.so /bin/turso_stress --silent --nr-iterations 10000
+LD_PRELOAD=/usr/lib/unreliable-libc.so RUST_BACKTRACE=1 RUST_LOG=debug /bin/turso_stress --silent --nr-iterations 10000
